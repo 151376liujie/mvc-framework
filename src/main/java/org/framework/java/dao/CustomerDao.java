7ab75a1f08888207f815bee4cadfddd9e7a70296
151376liujie <980463316@qq.com>
2016-04-01 00:06:28 +0800

    提交数据库工具类及其测试用例

---
+++ src/main/java/org/framework/java/dao/CustomerDao.java
@@ -0,0 +1,13 @@
+package org.framework.java.dao;
+
+import org.framework.java.model.Customer;
+
+public class CustomerDao {
+
+
+    public int insertCustomer(Customer customer) {
+	return 0;
+
+    }
+
+}

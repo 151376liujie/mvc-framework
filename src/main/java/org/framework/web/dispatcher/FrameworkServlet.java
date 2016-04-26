996085987f71a07cef9009be443fc1c12ad830c7
LiuJie <980463316@qq.com>
2016-04-26 15:50:24 +0800

    代码格式化

--- src/main/java/org/framework/web/dispatcher/FrameworkServlet.java
+++ src/main/java/org/framework/web/dispatcher/FrameworkServlet.java
@@ -1,14 +1,14 @@
 package org.framework.web.dispatcher;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Method;
-import java.nio.charset.Charset;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import org.apache.commons.lang3.StringUtils;
+import org.framework.BeanContainer;
+import org.framework.bean.ActionHandler;
+import org.framework.model.PageView;
+import org.framework.model.RequestParameter;
+import org.framework.model.ResponseData;
+import org.framework.utils.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -18,22 +18,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.StringUtils;
-import org.framework.BeanContainer;
-import org.framework.bean.ActionHandler;
-import org.framework.model.PageView;
-import org.framework.model.RequestParameter;
-import org.framework.model.ResponseData;
-import org.framework.utils.CodecUtils;
-import org.framework.utils.ConfigUtils;
-import org.framework.utils.ControllerUtils;
-import org.framework.utils.FrameworkLoader;
-import org.framework.utils.JsonUtils;
-import org.framework.utils.ReflectionUtils;
-import org.framework.utils.StreamUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * 核心拦截器，拦截所有请求
@@ -48,123 +41,123 @@ public class FrameworkServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
 
-	LOGGER.info(" prepare to init class... ");
-	// 初始化工作
-	FrameworkLoader.init();
+        LOGGER.info(" prepare to init class... ");
+        // 初始化工作
+        FrameworkLoader.init();
 
-	LOGGER.info(" load class finished.. ");
-	ServletContext servletContext = config.getServletContext();
-	// 注册处理jsp的servlet
-	ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
+        LOGGER.info(" load class finished.. ");
+        ServletContext servletContext = config.getServletContext();
+        // 注册处理jsp的servlet
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
 
-	String appViewPath = ConfigUtils.getAppViewPath();
-	jspServlet.addMapping(appViewPath + "*");
-	LOGGER.info("registe jsp servlet to servlet context..");
-	// 注册默认的servlet
-	ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+        String appViewPath = ConfigUtils.getAppViewPath();
+        jspServlet.addMapping(appViewPath + "*");
+        LOGGER.info("registe jsp servlet to servlet context..");
+        // 注册默认的servlet
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
 
-	String appWebResourcePath = ConfigUtils.getAppWebResourcePath();
-	defaultServlet.addMapping(appWebResourcePath + "*");
-	LOGGER.error("registe default servlet to servlet context..");
+        String appWebResourcePath = ConfigUtils.getAppWebResourcePath();
+        defaultServlet.addMapping(appWebResourcePath + "*");
+        LOGGER.error("registe default servlet to servlet context..");
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-	    IOException {
-	request.setCharacterEncoding("UTF-8");
-	response.setCharacterEncoding("UTF-8");
-	String requestURl = request.getPathInfo();
+            IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String requestURl = request.getPathInfo();
 
-	String method = request.getMethod().toLowerCase();
-	LOGGER.info("request url :{},request method :{}..", requestURl, method);
-	ActionHandler actionHandler = ControllerUtils.getActionHandler(requestURl, method);
-	if (actionHandler != null) {
-	    Method actionMethod = actionHandler.getActionMethod();
-	    Class<?> controllerClass = actionHandler.getControllerClass();
-	    Object controller = BeanContainer.getBean(controllerClass);
-	    // 获取请求参数
-	    Map<String, Object> map = getParameterMap(request);
-	    String body = CodecUtils.decodeUrl(StreamUtils.getString(request.getInputStream()));
-	    if (StringUtils.isNotEmpty(body)) {
-		String[] split = StringUtils.split(body, "&");
-		if (split.length > 0) {
-		    for (String param : split) {
-			String[] arr = StringUtils.split(param, "=");
-			if (arr.length == 2) {
-			    String paramName = arr[0];
-			    String paramValue = arr[1];
-			    map.put(paramName, paramValue);
-			}
-		    }
-		}
-	    }
-	    try {
-		RequestParameter requestParameter = new RequestParameter(map);
-		Object result = ReflectionUtils.invokeMethod(controller, actionMethod, requestParameter);
-		if (result instanceof PageView) {
-		    // 返回视图
-		    PageView view = (PageView) result;
-		    String path = view.getLocation();
-		    Map<String, Object> modelMap = view.getModelMap();
-		    if (StringUtils.isNotEmpty(path)) {
-			if (path.startsWith("/")) {
-			    response.sendRedirect(request.getContextPath() + path);
-			} else {
-			    for (Entry<String, Object> entry : modelMap.entrySet()) {
-				request.setAttribute(entry.getKey(), entry.getValue());
-			    }
-			    request.getRequestDispatcher(ConfigUtils.getAppViewPath() + path)
-				    .forward(request, response);
-			}
-		    }
-		} else if (result instanceof ResponseData) {
-		    // 返回json数据
-		    ResponseData responseData = (ResponseData) result;
-		    Object model = responseData.getModel();
-		    if (model != null) {
-			PrintWriter writer = response.getWriter();
-			response.setContentType("application/json");
-			response.setCharacterEncoding("UTF-8");
-			String jsonString = JsonUtils.toJsonString(model);
-			writer.write(jsonString);
-			writer.close();
-		    }
-		}
-	    } catch (Exception e) {
-		LOGGER.error(e.getMessage(), e);
-	    }
-	}
+        String method = request.getMethod().toLowerCase();
+        LOGGER.info("request url :{},request method :{}..", requestURl, method);
+        ActionHandler actionHandler = ControllerUtils.getActionHandler(requestURl, method);
+        if (actionHandler != null) {
+            Method actionMethod = actionHandler.getActionMethod();
+            Class<?> controllerClass = actionHandler.getControllerClass();
+            Object controller = BeanContainer.getBean(controllerClass);
+            // 获取请求参数
+            Map<String, Object> map = getParameterMap(request);
+            String body = CodecUtils.decodeUrl(StreamUtils.getString(request.getInputStream()));
+            if (StringUtils.isNotEmpty(body)) {
+                String[] split = StringUtils.split(body, "&");
+                if (split.length > 0) {
+                    for (String param : split) {
+                        String[] arr = StringUtils.split(param, "=");
+                        if (arr.length == 2) {
+                            String paramName = arr[0];
+                            String paramValue = arr[1];
+                            map.put(paramName, paramValue);
+                        }
+                    }
+                }
+            }
+            try {
+                RequestParameter requestParameter = new RequestParameter(map);
+                Object result = ReflectionUtils.invokeMethod(controller, actionMethod, requestParameter);
+                if (result instanceof PageView) {
+                    // 返回视图
+                    PageView view = (PageView) result;
+                    String path = view.getLocation();
+                    Map<String, Object> modelMap = view.getModelMap();
+                    if (StringUtils.isNotEmpty(path)) {
+                        if (path.startsWith("/")) {
+                            response.sendRedirect(request.getContextPath() + path);
+                        } else {
+                            for (Entry<String, Object> entry : modelMap.entrySet()) {
+                                request.setAttribute(entry.getKey(), entry.getValue());
+                            }
+                            request.getRequestDispatcher(ConfigUtils.getAppViewPath() + path)
+                                    .forward(request, response);
+                        }
+                    }
+                } else if (result instanceof ResponseData) {
+                    // 返回json数据
+                    ResponseData responseData = (ResponseData) result;
+                    Object model = responseData.getModel();
+                    if (model != null) {
+                        PrintWriter writer = response.getWriter();
+                        response.setContentType("application/json");
+                        response.setCharacterEncoding("UTF-8");
+                        String jsonString = JsonUtils.toJsonString(model);
+                        writer.write(jsonString);
+                        writer.close();
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
     }
 
     private Map<String, Object> getParameterMap(HttpServletRequest request) throws UnsupportedEncodingException {
-	Map<String, Object> map = new HashMap<String, Object>();
-	Enumeration<String> parameterNames = request.getParameterNames();
-	while (parameterNames.hasMoreElements()) {
-	    String parameterName = parameterNames.nextElement();
-	    String[] parameterValues = request.getParameterValues(parameterName);
-	    if (parameterValues != null && parameterValues.length > 0) {
-		if (parameterValues.length == 1) {
-		    String paramVal = new String(parameterValues[0].getBytes(Charset.forName("ISO-8859-1")),
-			    Charset.forName("utf-8"));
-		    map.put(parameterName, paramVal);
-		} else {
-		    StringBuilder sb = new StringBuilder();
-		    for (String pVal : parameterValues) {
-			String paramVal = new String(pVal.getBytes(Charset.forName("ISO-8859-1")),
-				Charset.forName("utf-8"));
-			sb.append(paramVal).append("@");
-		    }
-		    sb.deleteCharAt(sb.length() - 1);
-		    map.put(parameterName, sb.toString());
-		}
-	    }
-	}
-	return map;
+        Map<String, Object> map = new HashMap<String, Object>();
+        Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String parameterName = parameterNames.nextElement();
+            String[] parameterValues = request.getParameterValues(parameterName);
+            if (parameterValues != null && parameterValues.length > 0) {
+                if (parameterValues.length == 1) {
+                    String paramVal = new String(parameterValues[0].getBytes(Charset.forName("ISO-8859-1")),
+                            Charset.forName("utf-8"));
+                    map.put(parameterName, paramVal);
+                } else {
+                    StringBuilder sb = new StringBuilder();
+                    for (String pVal : parameterValues) {
+                        String paramVal = new String(pVal.getBytes(Charset.forName("ISO-8859-1")),
+                                Charset.forName("utf-8"));
+                        sb.append(paramVal).append("@");
+                    }
+                    sb.deleteCharAt(sb.length() - 1);
+                    map.put(parameterName, sb.toString());
+                }
+            }
+        }
+        return map;
     }
 
     @Override
     public void destroy() {
-	LOGGER.info("the web container is going to stop...");
+        LOGGER.info("the web container is going to stop...");
     }
 
 }
